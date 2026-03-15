f5512d0aa8a3029ee92141c97eb09c3f6663ca96
yossei-crypto <davigomespereira32@gmail.com>
2026-03-15 08:45:28 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -90,4 +90,7 @@ def nova_conversa():
     return jsonify({"id": nid})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # O segredo para o Render não dar erro de porta está aqui:
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
