f8ead4b9bb779a244c0eb85b715908d84c3c766a
yossei-crypto <davigomespereira32@gmail.com>
2026-03-15 07:14:23 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -110,5 +110,5 @@ def nova_conversa():
 
 # CONFIGURAÇÃO DE PORTA DINÂMICA PARA O RENDER
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
